3933f2f13f8d2749835f365f354a5f4ba79cb40d
Crowdbotics <team@crowdbotics.com>
2024-02-05 23:02:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "family-planner-46019",
+  url: "https://family-planner-46019.botics.co",
+  api: "https://family-planner-46019.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
